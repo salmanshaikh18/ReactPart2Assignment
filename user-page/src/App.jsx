62daed0d0aba8ab7315eb78f509bd34df831404a
salmanshaikh18 <salmanshaikh18786@gmail.com>
2023-08-07 10:32:24 +0530

    logic for sign in or signup

---
+++ user-page/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+import './App.css'
+
+import logo from './assets/instagram.png'
+
+function App() {
+  const [login, setLogin] = useState(true)
+
+  const loginSwitch = () => {
+    setLogin(!login)
+  }
+  return (
+    <div id='Container'>
+      <div id='user'>
+
+        <img src={logo} alt="logo" />
+
+        <input hidden={login} type="text" placeholder='Mobile Number or Email' />
+
+        <input hidden={login} type="text" placeholder='Full Name' />
+
+        <input type="email" placeholder='Phone Number, Username, or Email' />
+
+        <input type="text" placeholder='Password'/>
+
+        <button>{login ? "Sign In" : "Sign Up" }</button>
+      
+      <div id="footer">
+        {login ? "Don't have an accound?" : "have an accound?"} <span onClick={loginSwitch}>{login ? "Sign Up": "Log In"}</span>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
